49997521f041f98e12d78f62407c6bc448512a72
Elias Pozidis <pozidis.elias@hotmail.com>
2020-03-03 23:14:18 +0000

    Removed testes and the environment is ready

--- db/migrate/20200303230552_create_homes.rb
+++
@@ -1,9 +0,0 @@
-class CreateHomes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :homes do |t|
-      t.string :home
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_230552) do
-
-  create_table "homes", force: :cascade do |t|
-    t.string "home"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
