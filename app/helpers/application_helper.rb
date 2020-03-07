b3ce32a6f9d600ee5e16ba3f772c9e94bf2e1552
Elias Pozidis <pozidis.elias@hotmail.com>
2020-03-07 01:49:11 +0000

    Added Email Extension Validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
+require 'httparty'
+
 module ApplicationHelper
+
+  def getUKUniversities
+    url = 'http://universities.hipolabs.com/search?country=united kingdom'
+    response = HTTParty.get(url)
+    json = response.parsed_response
+    ukdomains = []
+    for i in 0..json.length-1 do
+      ukdomains << json[i]["domains"]
+    end
+    return ukdomains
+  end
+
+  def Validate(user_domain)
+    domains = getUKUniversities()
+    checkDomain = false
+    for i in 0..domains.length-1 do
+      if domains[i].include?(user_domain)
+        return true
+      end
+    end
+    return checkDomain
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :emailValidation
+
+  private
+
+    def emailValidation()
+      if ApplicationController.helpers.Validate(email)
+
+      else
+        errors.add(:email, "Is not valid")
+      end
+    end
 end
